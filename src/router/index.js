df2c0155264048ec40bdf545888aa316d1d712d7
Aleks <ownlocalstorage@gmail.com>
2024-05-14 10:47:49 +0300

    add NotFound page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/login'
+    },
+    {
       path: '/login',
       name: 'login',
       component: LoginView
@@ -12,10 +16,12 @@ const router = createRouter({
     {
       path: '/game',
       name: 'game',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/GameView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: () => import('../views/NotFound.vue')
     }
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="fill-height">
+    <v-row align="center" justify="center" no-gutters>
+      <v-col class="d-flex justify-center">
+        <h1 class="text-h1">Page not found</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
